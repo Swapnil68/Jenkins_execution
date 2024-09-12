a9d81e40725613c4665f2654d6771a5e5e40fc13
Swapnil68 <swapnilmahajan68@gmail.com>
2024-09-12 20:31:06 +0530

    Commit Code Swapnil first commit

---
+++ src/test/java/Execution01/CodeExceute.java
@@ -0,0 +1,16 @@
+package Execution01;
+
+import org.testng.annotations.Test;
+
+public class CodeExceute {
+	
+    @Test
+	public void test()
+	{
+		for(int i=1;i<=2;i++)
+		   {
+		     System.out.println(i);
+		    }
+
+}
+}
